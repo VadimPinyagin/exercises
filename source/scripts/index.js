d5b25275f03ba929f0d176c40df19f2916cb87bc
VadimPinyagin <vadimbox85@gmail.com>
2026-06-17 19:54:49 +0300

    Добавляет скрипт для новых грядок, перемещает кнопку вверх над грядками

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const button = document.querySelector('.garden__group-button');
+const garden = document.querySelector('.fox-garden');
+
+button.addEventListener('click', () => {
+
+  const currentBeds = garden.querySelectorAll('.fox-garden__bed');
+  const nextNumber = currentBeds.length + 1;
+
+  const newBed = document.createElement('div');
+  newBed.classList.add('fox-garden__bed');
+  newBed.textContent = nextNumber;
+
+  garden.appendChild(newBed);
+});
