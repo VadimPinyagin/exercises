999c18e3c1159f09352f53258845cb3356e54a87
VadimPinyagin <vadimbox85@gmail.com>
2026-06-28 04:31:50 +0300

    Добавляет кнопки для добавления/удаления цветочных грядок, добавляет фунцию для этих же цлей. добавляет свойство для каждой 5 грядки со смещением

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,21 +1,39 @@
-const garden = document.querySelector('.fox-garden');
+const foxGarden = document.querySelector('.fox-garden');
+const flowerGarden = document.querySelector('.flower-garden');
 const gardenControls = document.querySelector('.garden__button');
 
 const handleGardenActions = (e) => {
   const button = e.target.closest('.button');
   if (!button) return;
 
-  if (button.classList.contains('garden__button-add')) {
-    const currentBeds = garden.querySelectorAll('.fox-garden__bed');
+  if (button.classList.contains('garden__button-add-flowerbed')) {
+    const currentFlowerBeds = flowerGarden.querySelectorAll('.flower-garden__flowerbed');
+    const newflowerBed = document.createElement('div');
+
+    newflowerBed.classList.add('flower-garden__flowerbed');
+    newflowerBed.textContent = currentFlowerBeds.length + 1;
+    flowerGarden.appendChild(newflowerBed);
+  }
+
+  if (button.classList.contains('garden__button-remove-flowerbed')) {
+    const currentFlowerBeds = flowerGarden.querySelectorAll('.flower-garden__flowerbed');
+
+    if (currentFlowerBeds.length > 0) {
+      currentFlowerBeds[currentFlowerBeds.length - 1].remove();
+    }
+  }
+
+  if (button.classList.contains('garden__button-add-gardenbed')) {
+    const currentBeds = foxGarden.querySelectorAll('.fox-garden__bed');
     const newBed = document.createElement('div');
 
     newBed.classList.add('fox-garden__bed');
     newBed.textContent = currentBeds.length + 1;
-    garden.appendChild(newBed);
+    foxGarden.appendChild(newBed);
   }
 
-  if (button.classList.contains('garden__button-remove')) {
-    const currentBeds = garden.querySelectorAll('.fox-garden__bed');
+  if (button.classList.contains('garden__button-remove-gardenbed')) {
+    const currentBeds = foxGarden.querySelectorAll('.fox-garden__bed');
 
     if (currentBeds.length > 0) {
       currentBeds[currentBeds.length - 1].remove();
